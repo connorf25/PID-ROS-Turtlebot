4a3a6ec0035f81444450886c4245637b33f90fee
connorf25 <connorforbes25@gmail.com>
2021-03-22 19:44:18 +1000

    Added error_to_twist service to controller

--- src/assignment1/src/controller.cpp
+++ src/assignment1/src/controller.cpp
@@ -1,6 +1,7 @@
 #include "ros/ros.h"
 #include "assignment1/Sonars.h"
 #include "assignment1/SonarsToError.h"
+#include "assignment1/ErrorToTwist.h"
 
 class Controller {
   public:
@@ -10,16 +11,27 @@ class Controller {
 
 void Controller::sonarsCallback(const assignment1::Sonars sonars)
 {
+  // Calculate Error
   ros::ServiceClient errorClient = n.serviceClient<assignment1::SonarsToError>("sonars_to_error");
   assignment1::SonarsToError errorSrv;
   errorSrv.request.sonars = sonars;
   if (errorClient.call(errorSrv))
   {
     ROS_INFO("Distance: %li, Angle : %li", (long int)errorSrv.response.error.distance, (long int)errorSrv.response.error.angle);
+    // Calculate Twist
+    ros::ServiceClient twistClient = n.serviceClient<assignment1::ErrorToTwist>("error_to_twist");
+    assignment1::ErrorToTwist twistSrv;
+    twistSrv.request.error = errorSrv.response.error;
+    if (twistClient.call(twistSrv)) {
+      ROS_INFO("Speed: %f", twistSrv.response.twist.linear.x);
+    } else {
+      ROS_ERROR("Failed to call service ErrorToTwist");
+      return;
+    }
   }
   else
   {
-    ROS_ERROR("Failed to call service/s");
+    ROS_ERROR("Failed to call service SonarsToError");
     return;
   }
 }
