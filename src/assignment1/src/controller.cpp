fa2269a51737f53974e6508163e00f9eb468afad
Connor Forbes <connorforbes25@gmail.com>
2021-03-25 11:43:41 +1000

    fix: Publish command now works

--- src/assignment1/src/controller.cpp
+++ src/assignment1/src/controller.cpp
@@ -1,4 +1,5 @@
 #include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
 #include "assignment1/Sonars.h"
 #include "assignment1/SonarsToError.h"
 #include "assignment1/ErrorToTwist.h"
@@ -6,48 +7,65 @@
 class Controller {
   public:
     ros::NodeHandle n;
+    assignment1::Sonars lastReading;
+    // Callback for sonars subscriber assigns message to Sonars
     void sonarsCallback(const assignment1::Sonars);
+    // Calculate twist for publisher
+    geometry_msgs::Twist calcTwist();
 };
 
 void Controller::sonarsCallback(const assignment1::Sonars sonars)
 {
+  lastReading = sonars;
+}
+
+geometry_msgs::Twist Controller::calcTwist() {
+  geometry_msgs::Twist return_twist;
   // Calculate Error
   ros::ServiceClient errorClient = n.serviceClient<assignment1::SonarsToError>("sonars_to_error");
   assignment1::SonarsToError errorSrv;
-  errorSrv.request.sonars = sonars;
+  errorSrv.request.sonars = lastReading;
   if (errorClient.call(errorSrv))
   {
-    ROS_INFO("Distance: %li, Angle : %li", (long int)errorSrv.response.error.distance, (long int)errorSrv.response.error.angle);
     // Calculate Twist
     ros::ServiceClient twistClient = n.serviceClient<assignment1::ErrorToTwist>("error_to_twist");
     assignment1::ErrorToTwist twistSrv;
     twistSrv.request.error = errorSrv.response.error;
     if (twistClient.call(twistSrv))
     {
-      ROS_INFO("Speed: %f", twistSrv.response.twist.linear.x);
-      // Publish Twist
-      ros::Publisher twist_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
-      twist_pub.publish(twistSrv.response.twist);
+      return_twist = twistSrv.response.twist;
+      return return_twist;
     } 
     else 
     {
       ROS_ERROR("Failed to call service ErrorToTwist");
-      return;
+      return return_twist;
     }
   }
   else
   {
     ROS_ERROR("Failed to call service SonarsToError");
-    return;
+    return return_twist;
   }
 }
 
 int main(int argc, char **argv)
 {
+  // Init
   ros::init(argc, argv, "controller");
   Controller controller;
+  // Subscribe to sonars
   ros::Subscriber sub = controller.n.subscribe("sonars", 1000, &Controller::sonarsCallback, &controller);
-  ros::spin();
-
+  // Calculate twist and publish
+  ros::Publisher twist_pub = controller.n.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
+  while(ros::ok()) {
+    // Calculate Twist
+    geometry_msgs::Twist twist_command = controller.calcTwist();
+    ROS_INFO("Speed: %f %f %f", twist_command.linear.x, twist_command.linear.y, twist_command.linear.z);
+    ROS_INFO("Angle: %f %f %f", twist_command.angular.x, twist_command.angular.y, twist_command.angular.z);
+    // Publish message
+    twist_pub.publish(twist_command);
+    ros::spinOnce();
+  }
   return 0;
 }
